570cfa507bfa5a12dc770a1c894217c90304722e
MorphSight <irishsnowday@icloud.com>
2020-10-03 14:28:26 -0700

    Testing

--- template.php
+++ template.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('_View\HeaderPage.php');
+require_once('\_View\HeaderPage.php');
 //require_once('_App\ApplicationController.php');
 //require_once('_App\TemplateManager.php');
 //require_once ('_App\ScreenSwitcher.php');
