0eac25b5b5f487f8b276f4f27bda37c50ac16dff
MorphSight <irishsnowday@icloud.com>
2020-10-03 14:26:58 -0700

    Added composer file.

--- template.php
+++ template.php
@@ -1,31 +1,32 @@
 <?php
 
-require_once('_View\HeaderPage.php');
-require_once('_App\ApplicationController.php');
-require_once('_App\TemplateManager.php');
-require_once ('_App\ScreenSwitcher.php');
+//require_once('_View\HeaderPage.php');
+//require_once('_App\ApplicationController.php');
+//require_once('_App\TemplateManager.php');
+//require_once ('_App\ScreenSwitcher.php');
 //$header = new HeaderPage("Online Store");
 
-try {
+/*try {
     $currentScreen = ApplicationController::getCurrentScreen();
     $currentScreen->SetupScreen();
 } catch (Exception $e) {
     echo $e;
-}
+}*/
 
 ?>
 <!DOCTYPE HTML>
 <html>
     <?php
-        TemplateManager::GetHeader()->DisplayHeader();
+        //TemplateManager::GetHeader()->DisplayHeader();
     ?>
 <body>
     <div class ="container">
     <?php
-        TemplateManager::GetBreadcrumb()->Display();
-        TemplateManager::GetTitleDisplay()->Display();
-        ApplicationController::getCurrentScreen()->Display();
+        //TemplateManager::GetBreadcrumb()->Display();
+        //TemplateManager::GetTitleDisplay()->Display();
+        //ApplicationController::getCurrentScreen()->Display();
         //echo file_get_contents(ApplicationController::$currentScreen);
+        echo "This Works!!";
     ?>
     </div>
 </body>
