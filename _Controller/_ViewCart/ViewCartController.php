8164c98b23c3046a47d9fc149512bfd22ee8a2b7
MorphSight <irishsnowday@icloud.com>
2020-10-03 21:08:44 -0700

    Added cart logic

--- _Controller/_ViewCart/ViewCartController.php
+++ _Controller/_ViewCart/ViewCartController.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/_Controller/PageController.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/_Model/Cart.php');
 
 class ViewCartController extends PageController
 {
     public function SetupScreen()
     {
-        // TODO: Implement SetupScreen() method.
+        TemplateManager::GetTitleDisplay()->setTitleText("Your Cart");
+        TemplateManager::GetTitleDisplay()->setSubtitleText('Total is $' . Cart::TotalCost());
     }
 
     public function GetDisplayScript()
--- _Model/Cart.php
+++ _Model/Cart.php
@@ -3,9 +3,27 @@
 
 class Cart
 {
-    public $items;
+    private static $items = array();
 
-    public function TotalCost() {
+    public static function TotalCost() {
 
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getItems()
+    {
+        return self::$items;
+    }
+
+    public static function addItem($item){
+        array_push(static::$items, $item);
+    }
+
+    public static function removeItem($item){
+        if ($key = array_search($item->name, static::$items) != false){
+            unset($item);
+        }
     }
 }
